d1025e6f6a0ff71deafca2970b66e3ce09534a56
Michael Bilstein <Micha3lbilstein@gmail.com>
2016-02-27 22:19:02 -0500

    Added documentation

--- src/QuackCryptor.java
+++ src/QuackCryptor.java
@@ -12,42 +12,30 @@ public class QuackCryptor{
 
         Scanner scan;
         PrintWriter writer;
-        String command = args[0];
+        String command = args[0]; //used to specify the user's intent
         String fileName = args[1];
-        String temp;
-        String temp2;
+        String temp, temp2;
 
         if (command.equalsIgnoreCase("encrypt")) {
             try {
                 System.out.println("File: " + fileName);
                 File file = new File(fileName);
-                //scan = new Scanner(file);
                 FileInputStream fis = new FileInputStream(file);
-                int bytes = fis.available();
-                writer = new PrintWriter("quacked.quack", "UTF-8");
-                String header = Headers.makeHeader(fileName);
-                writer.print(header);
+                int bytes = fis.available(); //sees how many bytes are available in the file
+                writer = new PrintWriter("encrypted.quack", "UTF-8");
+                String header = Headers.makeHeader(fileName); //Creates the header, which stores data about file name/extension
+                writer.print(header); //Prints header to the start of the file
 
                 int b;
                 String toEncrypt = "";
                 for (int x = 0; x < bytes; x++) {
-                    System.out.println(b = fis.read());
-                    toEncrypt += b + " ";
-                    /*temp = AES.encrypt("QuackQuackQuack!", "RandomInitVector", ((char) b) + "");
-                    System.out.println(temp);
-                    temp2 = Quack.quackCrypt(temp);
-                    writer.print(temp2);*/
+                    b = fis.read(); //reads the next byte in the file
+                    toEncrypt += b + " "; //puts the next byte in the file followed by a space
                 }
-                temp = AES.encrypt("QuackQuackQuack!", "RandomInitVector", toEncrypt);
-                temp2 = Quack.quackCrypt(temp);
-                writer.print(temp2);
+                temp = AES.encrypt("QuackQuackQuack!", "RandomInitVector", toEncrypt); //encrypts the string of bytes
+                temp2 = Quack.quackCrypt(temp); //encrypts the encrypted string into quacks
+                writer.print(temp2); //prints quacks to the file, following the header
                 fis.close();
-                /*while(scan.hasNextByte()) {
-                    System.out.println("Found a byte!");
-                    temp = AES.encrypt("QuackQuackQuack!", "RandomInitVector", ((char) scan.nextByte()) + ""); //converts current byte to AES
-                    temp2 = Quack.quackCrypt(temp);
-                    writer.print(temp2);
-                }*/
                 writer.close();
 
             } catch (IOException e) {
@@ -59,40 +47,29 @@ public class QuackCryptor{
                 File file = new File(fileName);
                 scan = new Scanner(file);
                 if (!(scan.next().equals("qUACkquaCk"))) {
-                    System.out.println("Inputted file is not encrypted with QuackCryption");
+                    System.out.println("Inputted file is not encrypted with QuackCryption"); //returns if the encryption identifier is not at the start of the file
                     return;
                 }
-                String header = "";
+                String header = "";//creates a string to contain the header
                 for (int x = 0; x < 255; x++) {
-                    header += scan.next();
+                    header += scan.next();//add each quack to the header string
                     if (x != 254)
-                        header += " ";
+                        header += " ";//only add a space if not at the end
                 }
-                String name = Headers.decryptHeader(header);
-                System.out.println("name is: " + name);
-                String quacks = "";
+                String name = Headers.decryptHeader(header);//decrypt header to filename.extension
+                String quacks = "";//string used to store quacks
                 while (scan.hasNext()) {
-                    quacks += scan.next() + " ";
+                    quacks += scan.next() + " "; //contains all quacks in the file
                 }
-                String toUnAES = Quack.quackReader(quacks);
-                String unAESd = AES.decrypt("QuackQuackQuack!", "RandomInitVector", toUnAES);
-                System.out.println(unAESd);
+                String toUnAES = Quack.quackReader(quacks);//converts quacks to encrypted text
+                String unAESd = AES.decrypt("QuackQuackQuack!", "RandomInitVector", toUnAES);//decrypts encrypted text
+                //System.out.println(unAESd);
                 String[] test = unAESd.split(" ");
 
-
-                /*
-                String aes = Quack.quackReader(quacks);
-                String endString = "";
-                for (int x = 0; x < 1000; x++) {
-
-                    aes = AES.decrypt("QuackQuackQuack!", "RandomInitVector", aes);
-                }
-                */
-                File output = new File("icon.jpg");
+                File output = new File(name);//writes to a file with the name and extension found in the header
                 FileOutputStream fos = new FileOutputStream(output);
                 for (int i = 0; i < test.length; i++) {
-                    //System.out.println("50");
-                    fos.write(Integer.parseInt(test[i]));
+                    fos.write(Integer.parseInt(test[i]));//writes each character to the file
                 }
                 fos.close();
                 return;
@@ -100,7 +77,7 @@ public class QuackCryptor{
                 e.printStackTrace();
             }
         } else {
-            System.out.println("Usage: java quackcryption [encrypt, decrypt] [file.txt]");
+            System.out.println("Usage: java quackcryption [encrypt, decrypt] [file.txt]"); //if neither encrypt or decrpyt were selected, return
             return;
         }
     }
